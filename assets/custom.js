15be9b90e199efee2223b5381d890de35f593367
Sheena Garg <sheena.garg@imaginemarketingindia.com>
2023-01-23 15:26:59 +0530

    kalles theme downloaded

---
+++ assets/custom.js
@@ -0,0 +1,10 @@
+jQuery_T4NT(document).ready(function($) {
+
+     /**
+     * Variant selection changed
+     */
+	  $( document ).on( "variant:change", function( evt, variant ) {
+	    //console.log( variant );
+	  });
+
+});
